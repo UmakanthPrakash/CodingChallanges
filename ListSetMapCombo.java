a91772907b9d87d323942f955dc7a90ceab626f3
Umakanth <umakanth.prakash@gmail.com>
2021-05-13 10:27:57 +0400

    Update ListSetMapCombo.java

--- ListSetMapCombo.java
+++ ListSetMapCombo.java
@@ -25,17 +25,19 @@ public class ListSetMapCombo {
 
 //print data in set
         treesetdata.stream().forEach(System.out::println);
-        System.out.println("---- Above is set data ----");
+        p("---- Above is set data ----");
 
+        
 //print data in map
         treemapdata.forEach((key,value) -> System.out.println(key+" "+value));
-        System.out.println("---- Above is tree data ----");
+        p("---- Above is tree data ----");
 
+        
 //print mapdata in sorted - Ofcourse we are using treemap this is not required in case we are using hashmap this would be useful
         treemapdata.entrySet().stream()
                 .sorted( (k1,k2) -> k1.getValue().compareTo(k2.getValue()) )
                 .forEach((k -> System.out.println(k.getKey() +" "+k.getValue())));
-        System.out.println("---- Above is tree map data sorted by value ----");
+        p("---- Above is tree map data sorted by value ----");
 
 //Ascending Sorting by value and storing the result in Map
         Map<String, Integer> valueascsortedmap = treemapdata.entrySet().stream()
@@ -44,7 +46,7 @@ public class ListSetMapCombo {
                                                                     (e1, e2) -> e1 , LinkedHashMap::new)
                                                         );
         valueascsortedmap.forEach((key,value) -> System.out.println(key+" "+value));
-        System.out.println("---- Above is stored map data sorted by value ----");
+        p("---- Above is stored map data sorted by value ----");
 
 //Descending Sorting by value and storing the result in Map
         Map<String,Integer> valuedescsortedmap = treemapdata.entrySet().stream()
@@ -53,8 +55,7 @@ public class ListSetMapCombo {
                                         (e1,e2) -> e1, LinkedHashMap::new)
                 );
         valuedescsortedmap.forEach((key,value) -> System.out.println(key+" "+value));
-
-        System.out.println("---- Above is reverse stored map data sorted by value ----");
+        p("---- Above is reverse stored map data sorted by value ----");
 
     }
 
@@ -68,5 +69,9 @@ public class ListSetMapCombo {
         listdata.add("value7");listdata.add("value7");listdata.add("value7");listdata.add("value7");listdata.add("value7");listdata.add("value7");
         listdata.add("value5");listdata.add("value5");listdata.add("value5");listdata.add("value5");listdata.add("value5");listdata.add("value5");
         listdata.add("value8");listdata.add("value8");listdata.add("value8");
+    }
+    
+    public static void p(String input){
+        System.out.println(input);
     }
 }
